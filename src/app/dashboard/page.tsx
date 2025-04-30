e6f15e6d71d93f552f6488decfc75402fea155e4
kuca7a <jakubkucala@gmail.com>
2025-04-30 22:54:21 +0100

    delete router on dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,11 +2,9 @@
 
 import { useAuth0 } from "@auth0/auth0-react";
 import { useEffect } from "react";
-import { useRouter } from "next/navigation";
 
 const DashboardPage = () => {
   const { isAuthenticated, loginWithRedirect, logout, user } = useAuth0();
-  const router = useRouter();
 
   useEffect(() => {
     if (!isAuthenticated) {
